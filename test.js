49a90adf0e3e1f3b32e3784951eeba377dd9015c
PatilRohit957 <rohitpatil95702@gmail.com>
2025-09-16 01:15:54 +0530

    math

--- test.js
+++ test.js
@@ -2,11 +2,16 @@
 
 //console.log(typeof 56);
 
-let you = 123
+//let you = 123
  
-let stringyou = String(you)
+//let stringyou = String(you)
 
-console.log(typeof stringyou);
+//console.log(typeof stringyou);
 
+let name = 'rohit';
+
+let roll_no = 60;
+
+console.log(`My name is ${name} and roll no is ${roll_no}`);
 
 
