a2b32f337261dfb8231a0e395850ae1775b5b086
PatilRohit957 <rohitpatil95702@gmail.com>
2026-01-27 14:17:30 +0530

    DOM

---
+++ object.js
@@ -0,0 +1,16 @@
+  //const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+  //const newNums = myNumbers.map( (num) => {return num+10})
+  
+  //const newNums = myNumbers
+    //              .map((num) => num * 10)
+      //            .map((num) =>num+ 1)
+  //console.log(newNums);
+  
+  const myNums = [1,2,3]
+
+
+  const myTotal = myNums.reduce((acc, curr) => acc+curr, 0)
+
+  console.log(myTotal);
+  
